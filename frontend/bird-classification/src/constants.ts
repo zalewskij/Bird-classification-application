98ea541a3e7b57ab92f838187df25cf649a17c68
Piotr Sieczka <siekaczp@gmail.com>
2023-12-27 17:49:41 +0100

    Constants corrected

--- frontend/bird-classification/src/constants.ts
+++ frontend/bird-classification/src/constants.ts
@@ -1,3 +1,3 @@
 export const PRIMARY_COLOR = '#3c716b';
-export const BACKEND_URL = 'http://127.0.0.1:5000/analyze-audio';
-// export const BACKEND_URL = 'https://birdclassification.pythonanywhere.com/analyze-audio';
+export const BACKEND_URL = 'https://birdclassification.pythonanywhere.com/analyze-audio';
+// export const BACKEND_URL = 'http://127.0.0.1:5000/analyze-audio';
